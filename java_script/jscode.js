bad39a65f48ab4cf14aac512aa3c0f5ccd1f0bc3
vinayjallipalli <vinayjallipalli@gmail.com>
2016-12-12 15:02:16 +0530

    first commit

---
+++ java_script/jscode.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('.navbar-default');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.navbar-default').css('background-color', 'rgba(34,34,34,2.0)');
+       } else {
+          $('.navbar-default').css('background-color', 'transparent');
+       }
+   });
+});
+
+
+
