6260356e22aa87ab380d2e596c806e8155d680bb
vinayjallipalli <vinayjallipalli@gmail.com>
2016-12-12 20:10:11 +0530

    fourth commit

--- java_script/jscode.js
+++ java_script/jscode.js
@@ -1,12 +1,10 @@
-
-
 $(document).ready(function(){       
    var scroll_start = 0;
    var startchange = $('.navbar-default');
    var offset = startchange.offset();
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
+      if(scroll_start > 100) {
           $('.navbar-default').css('background-color', 'rgba(34,34,34,2.0)');
        } else {
           $('.navbar-default').css('background-color', 'transparent');
